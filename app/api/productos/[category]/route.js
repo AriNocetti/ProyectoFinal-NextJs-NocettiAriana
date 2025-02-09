5346bea67bb066fa97990fb70ea125ec6755c462
AriNocetti <arinocetti@gmail.com>
2025-02-09 20:15:24 -0300

    fix error

--- app/api/productos/[category]/route.js
+++ app/api/productos/[category]/route.js
@@ -3,27 +3,27 @@ import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "@/configFirebase"
 
 export async function GET(request, { params }) {
-    try {
-        console.log("cp params", params);
-        const { category } = params;
+    const { category } = await params;
 
-        let productsCollection = collection(db, "productosRopa");
+    let productsCollection = collection(db, "productosRopa");
 
-        if (category !== "todos") {
-            console.log("cat =! todos", category);
-            productsCollection = query(productsCollection, where("category", "==", category));
-        }
-
-        // 🔹 Esperamos la respuesta de Firestore antes de devolver el resultado
-        const resp = await getDocs(productsCollection);
-        const res = resp.docs.map((elemento) => ({
-            ...elemento.data(),
-            id: elemento.id,
-        }));
-
-        return NextResponse.json(res, { status: 200 });
-    } catch (error) {
-        console.error("Error en la API:", error);
-        return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
+    if (category !== "todos") {
+        console.log("cat =! todos", category);
+        productsCollection = query(productsCollection, where("category", "==", category));
     }
+
+    // 🔹 Esperamos la respuesta de Firestore antes de devolver el resultado
+    const resp = await getDocs(productsCollection);
+    const res = resp.docs.map((elemento) => ({
+        ...elemento.data(),
+        id: elemento.id,
+    }));
+
+    return NextResponse.json(res, { status: 200 });
+    // try {
+
+    // } catch (error) {
+    //     console.error("Error en la API:", error);
+    //     return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
+    // }
 }
--- app/productos/[categoryName]/page.js
+++ app/productos/[categoryName]/page.js
@@ -1,8 +1,10 @@
 import ProductsFilter from "@/components/productsFilter/ProductsFilter";
 
 export default async function Productos({params}) {
-  const { categoryName } = params;
+  const { categoryName } = await params;
+  console.log(categoryName,'paramsss')
   const response = await fetch(`${process.env.NEXT_PUBLIC_NETLIFY_URL}/api/productos/${categoryName}`, { cache: "no-store" });
+  console.log(response, 'res')
   const items = await response.json();
 
   return(
