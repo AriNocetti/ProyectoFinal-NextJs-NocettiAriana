a587581dc0af88e70f8001e20dd17c460242bb2f
AriNocetti <arinocetti@gmail.com>
2025-02-11 12:02:51 -0300

    borrado de pagina create

--- app/NewProduct/page.js
+++ app/NewProduct/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import FormUploadNewProduct from "@/components/FormNewProduct/FormNewProduct";
 import ModalNewProduct from "@/components/FormNewProduct/ModalNewProduct";
 
 export default function NewProduct(){
