bc547416c4a0ebe150695363217496d56ad447bb
AriNocetti <arinocetti@gmail.com>
2025-02-12 17:46:48 -0300

    agregado de filtros

--- components/productsFilter/ProductsFilter.jsx
+++ components/productsFilter/ProductsFilter.jsx
@@ -8,6 +8,7 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { Slider } from '@mui/material';
 import ItemList from "@/components/ItemList/ItemList";
+
 const marks = [
     { value: 0, label: '$0', },
     { value: 7000, label: '$7000', },
@@ -15,41 +16,44 @@ const marks = [
     { value: 25000, label: '$25000', },
     { value: 35000, label: '$35000', },
 ];
-
 function valuetext(value) {
     return `$${value}`;
 }
 
 export default function ProductsFilter({ products }) {
-    // console.log('llegan', products)
 
-    // const [items, setItems] = useState(products);
+    const [filteredItems, setFilteredItems] = useState([products]);
 
-    // const [filteredItems, setFilteredItems] = useState(products);
-
-    // const [fabric, setFabric] = useState('');
-
-    // const handleChange = (event) => {
-    //     setFabric(event.target.value);
-    // };
-
-    // const [whatPrice, setWhatPrice] = useState([6000, 50000]);
-
-    // const handleWhatPrice = (event, newValue) => {
-    //     setWhatPrice(newValue);
-    // };
-
-    // useEffect(() => {
-    //     setFilteredItems(items.filter((item) => fabric == "" ? true : item.fabric == fabric))
-    // }, [fabric, items]);
-
-    // useEffect(() => {
-    //     setFilteredItems(items.filter((item) => item.price >= whatPrice[0] && item.price <= whatPrice[1]))
-    // }, [whatPrice, items])
-
+    
+    //filtro fabric
+    const [fabric, setFabric] = useState('');
+    
+    const handleFabric = (event) => {
+        setFabric(event.target.value);
+    };
+    
+    useEffect(() => {
+        let itemsFilteredByFabric = products.filter((item) => fabric == "" ? true : item.fabric == fabric)
+        let itemsFilteredByFabricAndPrice = itemsFilteredByFabric.filter((item) => item.price >= whatPrice[0] && item.price <= whatPrice[1])
+        setFilteredItems(itemsFilteredByFabricAndPrice)
+    }, [fabric, products]);
+    
+    //filtro rango precios
+    const [whatPrice, setWhatPrice] = useState([5000, 50000]);
+    
+    const handleWhatPrice = (event, newValue) => {
+        setWhatPrice(newValue);
+    };
+    
+    useEffect(() => {
+        let itemsFilteredByPrice = products.filter((item) => item.price >= whatPrice[0] && item.price <= whatPrice[1])
+        let itemsFilteredByPriceAndFabric = itemsFilteredByPrice.filter((item) => fabric == "" ? true : item.fabric == fabric)
+        setFilteredItems(itemsFilteredByPriceAndFabric)
+    }, [whatPrice, products])
+    
     return (
         <>
-        {/* <div className="flex flex-row gap-7">
+        <div className="flex flex-row gap-7">
             <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
                 <InputLabel id="demo-select-small-label">Tela</InputLabel>
                 <Select
@@ -57,8 +61,8 @@ export default function ProductsFilter({ products }) {
                     id="demo-select-small"
                     value={fabric}
                     label="Tela"
-                    onChange={handleChange}
-                >
+                    onChange={handleFabric}
+                    >
                     <MenuItem value="">
                         <em>None</em>
                     </MenuItem>
@@ -81,9 +85,34 @@ export default function ProductsFilter({ products }) {
                 max={35000}
                 step={null}
                 className="max-w-72"
-            />
-        </div> */}
-            <ItemList items={products} />
+                />
+        </div>
+            <ItemList items={filteredItems} />
         </>
     )
 }
+
+
+// const {categoryName} = useParams;
+
+// useEffect(() => {
+    //     let productsCollection = collection(db, "productosRopa");
+    //     let consulta = productsCollection;
+    
+    //     if (categoryName) {
+    //         let productsCollectionFiltered = query(
+    //             productsCollection,
+    //             where("category", "==", categoryName)
+    //         );
+    //         consulta = productsCollectionFiltered;
+    //     }
+        
+    //     getDocs(consulta).then((res) => {
+    //         let array = res.docs.map((elemento) => {
+    //         return {...elemento.data(), id: elemento.id};
+    //     });
+        
+    //     setItems(array);
+    //     setFilteredItems(array);
+    //     });
+    //     }, [categoryName]);
